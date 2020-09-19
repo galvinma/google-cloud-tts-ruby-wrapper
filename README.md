9f35b4f8e6c4fe8aaea69181b435d4aafa61bc6b
galvinma <matthew.galvin@providence.org>
2020-09-19 14:33:17 -0700

    Update convert class for bulk anki card creation

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 ### Convert text input to mp3 via Google Cloud TTS
 
-ex. ruby convert.rb
+Primary use case is creation of Anki vocab cards. Cards have native languagage, target lanugage, and audio fields. Current workflow:
+
+1. Create a CSV file of vocab words. For example:
+
+le poisson, fish
+le porc, pork
+le potage, soup
+
+2. Execute the following IRB**:
+- load 'convert.rb'
+- x = RubyGoogleTTS.new
+- x.create_anki_csv
+
+** Assumes macOS, Ruby 2.6+, and installed Gems
+
+This will 
+- Create an MP3 audio file for each item in the first column in the /output directory.
+- Create an, 'anki_import.csv' file in the /anki directory.
+
+
+3. Manually copy audio files from the output directory into anki collections folder (On macOS this is currently ~/Library/Application Support/Anki2/User/collection.media).
+4. Import CSV in anki mapping CSV fields to card fields.
+
--- convert.rb
+++ convert.rb
@@ -1,41 +1,96 @@
 require "google/cloud/text_to_speech"
 require "highline/import"
 require "dotenv/load"
+require "csv"
 
 Dotenv.load
 
-# Get text input
-input_text = ask "Input text: "
+class RubyGoogleTTS
+  attr_accessor :raw_list
+  attr_accessor :raw_csv
 
-# Instantiates a client
-client = Google::Cloud::TextToSpeech.text_to_speech
+  def initialize
+    @client = Google::Cloud::TextToSpeech.text_to_speech
+    @raw_list = File.read("input/input.txt")
+    @raw_csv = CSV.parse(File.read("input/input.csv"))
+  end
 
-hash = {
-  text: input_text,
-}
+  # Creates a csv file for anki import
+  # Expects a list of words in input.csv
+  #
+  # ex.
+  # le pain grillé, toast
+  # le pain au chocolat, pain au chocolat
+  # le pâté, pâté
+  #
+  # Will create an anki import csv with rows native, target, audio
+  # Place audio manually in media.collections
+  def create_anki_csv
+    file = "anki/anki_import.csv"
+    CSV.open(file, "w") do |writer|
+      @raw_csv.each do |row|
+        target = row[0]
+        native = row[1]
+        audio_file = convert(target)
+        anki_audio_syntax = "[sound:#{audio_file}]" # [sound:fr-FR_je t'aime aussi.mp3]
+        writer << [native, target, anki_audio_syntax]
+      end
+    end
+  end
 
-voice = {
-  language_code: ENV["LANGUAGE_CODE"].to_s,
-  ssml_gender: ENV["SSML_GENDER"].to_s,
-}
+  # Parses a comma delimted list
+  # Each target phrase is on a newline.
+  # Each lines ends with a comma
+  def parse_raw_list
+    @input_list = @raw_list.gsub(/[\r\n]+/, "").split(",").map { |x| x.to_s.strip }
+  end
 
-audio_config = {
-  audio_encoding: "MP3",
-  speaking_rate: ENV["SPEAKING_RATE"].to_f,
-}
+  # Accepts a list of phrases and calls convert function
+  def bulk_convert_list
+    parse_raw_list
 
-response = client.synthesize_speech(
-  input: hash,
-  voice: voice,
-  audio_config: audio_config,
-)
+    @input_list.each do |phrase|
+      convert(phrase)
+    end
+  end
 
-# Write the file
-output_path = "output/#{ENV["LANGUAGE_CODE"].to_s}_#{input_text}.mp3"
-File.open output_path, "wb" do |file|
-  file.write response.audio_content
-  puts "Audio content written to #{output_path}.mp3'"
+  # Accepts a phrase and writes an MP3 audio output
+  def convert(input_text = nil)
+    # Get text input
+    if input_text == nil
+      input_text = ask "Input text: "
+    end
+
+    hash = {
+      text: input_text,
+    }
+
+    voice = {
+      language_code: ENV["LANGUAGE_CODE"].to_s,
+      ssml_gender: ENV["SSML_GENDER"].to_s,
+    }
+
+    audio_config = {
+      audio_encoding: "MP3",
+      speaking_rate: ENV["SPEAKING_RATE"].to_f,
+    }
+
+    response = @client.synthesize_speech(
+      input: hash,
+      voice: voice,
+      audio_config: audio_config,
+    )
+
+    # Write the file
+    output_path = "output/#{ENV["LANGUAGE_CODE"].to_s}_#{input_text}.mp3"
+    filename = "#{ENV["LANGUAGE_CODE"].to_s}_#{input_text}.mp3"
+    File.open output_path, "wb" do |file|
+      file.write response.audio_content
+      puts "Audio content written to #{output_path}.mp3'"
+    end
+
+    # Play the file (macOS)
+    fork { exec "afplay", output_path }
+    filename
+  end
 end
-
-# Play the file (macOS)
-fork { exec "afplay", output_path }
